d46469e5a848a1f0d57089aaf3237ceed37abe93
bvd <bvduong76@gmail.com>
2017-01-17 20:37:40 +0700

    zxccc

--- 1312100Final/1312100Final.cpp
+++ 1312100Final/1312100Final.cpp
@@ -4,6 +4,10 @@
 #include <shellapi.h>
 #define MAX_LOADSTRING 100
 #define	WM_USER_SHELLICON WM_USER + 1
+#define WM_GLOBALHOOK WM_USER-1
+#define MAX_BUF_LENGTH 512
+#include <comdef.h>
+
 // Global Variables:
 HMENU hPopMenu;
 HINSTANCE hInst;                                // current instance
@@ -13,6 +17,22 @@ BOOL bDisable = FALSE;
 PTSTR pszCurDir;
 
 INT_PTR CALLBACK Dialog1_WndProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+INT_PTR CALLBACK Dialog2_WndProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+//1312100Final.cpp : Defines the entry point for the application.
+#include "stdafx.h"
+#include "1312100Final.h"
+#include <shellapi.h>
+#define MAX_LOADSTRING 100
+#define	WM_USER_SHELLICON WM_USER + 1
+#define WM_GLOBALHOOK WM_USER-1
+// Global Variables:
+TCHAR ordirinalCurDir[MAX_PATH];
+void doInstallHook(HWND hWnd);
+void doRemoveHook(HWND hWnd);
+
+
+
+
 NOTIFYICONDATA nidApp = {
 };
 // Forward declarations of functions included in this code module:
@@ -21,41 +41,41 @@ BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // TODO: Place code here.
+	// TODO: Place code here.
 
-    // Initialize global strings
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_MY1312100FINAL, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
+	// Initialize global strings
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDC_MY1312100FINAL, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
 
-    // Perform application initialization:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
+	// Perform application initialization:
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MY1312100FINAL));
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MY1312100FINAL));
 
-    MSG msg;
+	MSG msg;
 
-    // Main message loop:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
+	// Main message loop:
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
 
-    return (int) msg.wParam;
+	return (int)msg.wParam;
 }
 
 
@@ -66,48 +86,48 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 //
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
-    WNDCLASSEXW wcex;
-    wcex.cbSize = sizeof(WNDCLASSEX);
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MY1312100FINAL));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_MY1312100FINAL);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-    return RegisterClassExW(&wcex);
+	WNDCLASSEXW wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MY1312100FINAL));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_MY1312100FINAL);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+	return RegisterClassExW(&wcex);
 }
 
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // Store instance handle in our global variable
-   HICON hMainIcon;
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+	hInst = hInstance; // Store instance handle in our global variable
+	HICON hMainIcon;
+	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+	doInstallHook(hWnd);
+	if (!hWnd)
+	{
+		return FALSE;
+	}
+	hMainIcon = LoadIcon(hInstance, (LPCTSTR)MAKEINTRESOURCE(IDI_ICON1));
+	nidApp.uVersion = NOTIFYICON_VERSION_4;
+	nidApp.cbSize = sizeof(NOTIFYICONDATA);
+	nidApp.hWnd = (HWND)hWnd;
+	nidApp.uID = IDI_ICON1;
+	nidApp.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
+	nidApp.hIcon = hMainIcon;
+	nidApp.uCallbackMessage = WM_USER_SHELLICON;
+	LoadString(hInstance, NULL, nidApp.szTip, MAX_LOADSTRING);
+	Shell_NotifyIcon(NIM_ADD, &nidApp);
 
-   if (!hWnd)
-   {
-      return FALSE;
-   }
-   hMainIcon = LoadIcon(hInstance, (LPCTSTR)MAKEINTRESOURCE(IDI_ICON1));
-   nidApp.uVersion = NOTIFYICON_VERSION_4;
-   nidApp.cbSize = sizeof(NOTIFYICONDATA);
-   nidApp.hWnd = (HWND)hWnd;
-   nidApp.uID = IDI_ICON1;
-   nidApp.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
-   nidApp.hIcon = hMainIcon;
-   nidApp.uCallbackMessage = WM_USER_SHELLICON;
-   LoadString(hInstance, NULL, nidApp.szTip, MAX_LOADSTRING);
-   Shell_NotifyIcon(NIM_ADD, &nidApp);
+	//ShowWindow(hWnd, nCmdShow);
+	// UpdateWindow(hWnd);
 
-   //ShowWindow(hWnd, nCmdShow);
-  // UpdateWindow(hWnd);
-
-   return TRUE;
+	return TRUE;
 }
 //
 //  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
@@ -123,8 +143,12 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	int wmEvent;
 	POINT lpClickPoint;
-    switch (message)
-    {
+	switch (message)
+	{
+	case WM_GLOBALHOOK: {
+		DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG1), hWnd, Dialog2_WndProc);
+	}
+						break;
 	case WM_USER_SHELLICON:
 		//systray msg callback
 		switch (LOWORD(lParam))
@@ -137,8 +161,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			{
 				uFlag |= MF_GRAYED;
 			}
-			InsertMenu(hPopMenu, 0xFFFFFFFF, uFlag, ID_MENU_VIEWNOTE, _T("Test 2")); // Test 2
-			InsertMenu(hPopMenu, 0xFFFFFFFF, uFlag, ID_MENU_VIEWSTATISTICS, _T("Test 1")); // Test 1				
+			InsertMenu(hPopMenu, 0xFFFFFFFF, uFlag, ID_MENU_VIEWNOTE, _T("View Notes")); // Test 2
+			InsertMenu(hPopMenu, 0xFFFFFFFF, uFlag, ID_MENU_VIEWSTATISTICS, _T("View Statistics")); // Test 1				
 			InsertMenu(hPopMenu, 0xFFFFFFFF, MF_BYPOSITION | MF_STRING, IDM_EXIT, _T("Exit"));
 
 			SetForegroundWindow(hWnd);
@@ -147,8 +171,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 		}
 		break;
-    case WM_COMMAND:
-        {
+	case WM_COMMAND:
+	{
 		wmEvent = HIWORD(wParam);
 		int wmId = LOWORD(wParam);
 		// Parse the menu selections:
@@ -156,53 +180,53 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		{
 		case ID_MENU_VIEWNOTE:
 			//MessageBox(NULL, _T("This is a test for menu view note"), _T("Test 1"), MB_OK);
-			DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG1), 0, Dialog1_WndProc);
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), 0, Dialog1_WndProc);
 			break;
 		case ID_MENU_VIEWSTATISTICS:
 			MessageBox(NULL, _T("This is a test for menu statistics"), _T("Test 2"), MB_OK);
 			break;
 		case IDM_EXIT:
 			Shell_NotifyIcon(NIM_DELETE, &nidApp);
-            DestroyWindow(hWnd);
-			break;}
-        }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: Add any drawing code that uses hdc here...
-            EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
+			doRemoveHook(hWnd);
+			DestroyWindow(hWnd);
+			break;
+		}
+	}
+	break;
+	case WM_DESTROY:
+		doRemoveHook(hWnd);
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
 }
 
-// Message handler for about box.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
-
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
+void doInstallHook(HWND hWnd) {
+	typedef void(*MYPROC)(HWND);
+	HINSTANCE hinstLib;
+	MYPROC ProcAddr;
+	hinstLib = LoadLibrary(L"FinalDLL.dll");
+	if (hinstLib != NULL) {
+		ProcAddr = (MYPROC)GetProcAddress(hinstLib, "_doInstallHook");
+		if (ProcAddr != NULL)
+			ProcAddr(hWnd);
+	}
 }
+
+void doRemoveHook(HWND hWnd) {
+	typedef void(*MYPROC)(HWND);
+	HINSTANCE hinstLib;
+	MYPROC ProcAddr;
+	hinstLib = LoadLibrary(L"FinalDLL.dll");
+	if (hinstLib != NULL) {
+		ProcAddr = (MYPROC)GetProcAddress(hinstLib, "_doRemoveHook");
+		if (ProcAddr != NULL)
+			ProcAddr(hWnd);
+	}
+}
+
 
 //code xu ly hop thoai xuat hien khi nhap chon menu test 2.
 //hien thi cac file trong thu muc notestored
@@ -218,7 +242,7 @@ INT_PTR CALLBACK Dialog1_WndProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM 
 		wcscat(pszCurDir, L"NoteStored");
 		CreateDirectory(pszCurDir, 0);
 		wcscat(pszCurDir, L"\\");
-		//DlgDirList(hDlg, achBuffer, IDC_LIST1, 0, DDL_ARCHIVE | DDL_DIRECTORY);
+		DlgDirList(hDlg, achBuffer, IDC_LIST1, 0, DDL_ARCHIVE | DDL_DIRECTORY);
 		return FALSE;
 	case WM_COMMAND: {
 		switch (LOWORD(wParam)) {
@@ -236,4 +260,63 @@ INT_PTR CALLBACK Dialog1_WndProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM 
 					 break;
 	}
 	return FALSE;
+}
+
+
+
+INT_PTR CALLBACK Dialog2_WndProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
+	WCHAR* NoteBuf = new WCHAR[MAX_BUF_LENGTH];
+	WCHAR* TagBuf = new WCHAR[MAX_BUF_LENGTH];
+	FILE* f;
+	char* fileName;
+	TCHAR achBuffer[MAX_PATH];
+	switch (message)
+	{
+	case WM_INITDIALOG: {
+		if (NoteBuf != NULL)
+			delete[] NoteBuf;
+		if (TagBuf != NULL)
+			delete[] TagBuf;
+		if (pszCurDir != NULL)
+			pszCurDir = NULL;
+		return TRUE;
+	}
+						break;
+	case WM_COMMAND: {
+		switch (LOWORD(wParam))
+		{
+		case IDC_BUTTON1: {
+			GetDlgItemText(hDlg, IDC_EDIT1, NoteBuf, MAX_BUF_LENGTH);
+			GetDlgItemText(hDlg, IDC_EDIT2, TagBuf, MAX_BUF_LENGTH);
+			//write file
+			
+			pszCurDir = achBuffer;
+			GetCurrentDirectory(MAX_PATH, pszCurDir);
+			_tcscpy(ordirinalCurDir, pszCurDir);
+			wcscat(pszCurDir, L"NoteStored");
+			if (SetCurrentDirectory(pszCurDir)) {
+				wcscat(TagBuf, L".txt");
+				_bstr_t temp(TagBuf);
+				fileName = temp;
+				f = fopen(fileName, "a+");
+				fwprintf(f, L"%s", NoteBuf);
+				fclose(f);
+				SetCurrentDirectory(ordirinalCurDir);
+			}
+			else {
+				CreateDirectory(pszCurDir, 0);
+				SetCurrentDirectory(ordirinalCurDir);
+			}
+			EndDialog(hDlg, TRUE);
+		}
+				   break;
+		case IDCANCEL:
+			EndDialog(hDlg, TRUE);
+			break;
+		}
+	}
+					 break;
+	default:
+		return FALSE;
+	}
 }
